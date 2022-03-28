d318cb0b708424c6ad917ccc43dca88dc9d90bba
eymenyaci <eymenyaci@gmail.com>
2022-03-29 01:31:17 +0300

    test commit

---
+++ OrderCreatedIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public class OrderCreatedIntegrationEvent : IntegrationEvent
+{
+    public int Id { get; set; }
+
+    public OrderCreatedIntegrationEvent(int id)
+    {
+        Id = id;
+    }
+
+}
